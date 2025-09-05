96b9834ce6552a10c951e7de613a61e8439dbcea
GUGAPRIYAA <115546633+Gugasivakumar0206@users.noreply.github.com>
2025-09-05 13:39:24 +0530

    images.config.js

---
+++ src/images.config.js
@@ -0,0 +1,6 @@
+// client/src/images.config.js
+// Map slugs (from DB) to images stored in /public/images/hotels
+export default {
+  'seaside-retreat': ['hotel1.jpg', 'hotel3.jpg', 'hotel4.jpg'],
+  'mountain-view-inn': ['hotel2.jpg', 'hotel3.jpg'],
+};
